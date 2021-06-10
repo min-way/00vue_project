2d663eb4faf8b30f5f548446b355d2ea26d28824
min-way <nissyou@naver.com>
2021-06-10 15:16:37 +0900

    -

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <banner/>
+
+  <div class="black-bg" v-if="proView==true">
+    <div class="white-bg">
+      <img v-bind:src="product[proNum].image">
+      <div>{{product[proNum].title}}</div>
+      <div>{{product[proNum].price}}</div>
+      <div>{{product[proNum].content}}</div>
+      <button v-on:click="proView=false">닫기</button>
+    </div>
+  </div>
+  
+  <ul class="view">
+    <li v-for="(item,i) in product" :key="i">
+      <img v-bind:src="product[i].image">
+      <div>{{product[i].title}}<span v-on:click="proView=true,proNum=i">[상세보기]</span></div>
+      <div>{{product[i].price}}</div>
+    </li>
+  </ul>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import vdata from './data.js'
+import banner from './components/banner.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data(){
+    return{
+      proView:false,
+      product:vdata,
+    }
+  },
+ components:{
+  //  banner:banner, 키값이 같으면 한번만 가능
+   banner
+ }
+  
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  *{margin: 0;padding: 0;}
+  li{list-style: none;}
+  img {width: 100%;}
+  .view li {margin-bottom: 20px;}
+  
+  .black-bg {position: fixed; width: 100%; height: 100%; background: rgba(0,0,0,0.3); display: flex; justify-content: center; align-items: center;}
+  .white-bg {position: fixed; width: 300px; height: 400px; background: #fff; padding: 20px; border-radius: 10px;}
+  .white-bg img {width: 100%; margin-top: 20px;}
 </style>
